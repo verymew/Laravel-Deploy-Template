d1d5df249b0c63320f9a330f3d69be82fbfd3a3b
verymew <verymewfor@gmail.com>
2024-07-15 04:36:26 -0300

    relacionamentos

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'title',
+        'content',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
